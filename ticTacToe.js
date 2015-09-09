7c4ee097b2d174fa25547e29ee397ff86e12b187
Stephen Deddens <sdeddens@gmail.com>
2015-09-09 16:01:06 -0500

    identify that we have a winner locic working

--- ticTacToe.js
+++ ticTacToe.js
@@ -6,6 +6,13 @@ $(function(){
 // then drive the whole program from the onclick event!
 	
 	var xPlayerState = true;
+	
+	// create an object to store the field state so we can check for a winner;
+	var fldBtnState = {
+		x0: "click me", x1: "click me", x2: "click me",
+		x3: "click me", x4: "click me", x5: "click me",
+		x6: "click me", x7: "click me", x8: "click me",
+	};
 
 	$("button.field").on("click", function () {
 
@@ -13,16 +20,41 @@ $(function(){
 		var buttonId = $(this).attr('id');
 
 		button = $("#"+buttonId);
-		if ((button.html() === "X") || (button.html() === "Y")) {
+		if ((button.html() === "X") || (button.html() === "O")) {
 			return;	// square already played!
 		};
 
 		if (xPlayerState) {
 			button.html("X");
+			fldBtnState[buttonId]="X";
 			xPlayerState = false;
 		} else {
 			button.html("O");
+			fldBtnState[buttonId]="O";
 			xPlayerState = true;
 		};
+		// check for winner... brute force, no feniss!
+		if ( 
+			((fldBtnState.x0 === 'X') && (fldBtnState.x1 === 'X') && (fldBtnState.x2 === 'X')) ||
+			((fldBtnState.x3 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x5 === 'X')) ||
+			((fldBtnState.x6 === 'X') && (fldBtnState.x7 === 'X') && (fldBtnState.x8 === 'X')) ||
+			((fldBtnState.x0 === 'X') && (fldBtnState.x3 === 'X') && (fldBtnState.x6 === 'X')) ||
+			((fldBtnState.x1 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x7 === 'X')) ||
+			((fldBtnState.x2 === 'X') && (fldBtnState.x5 === 'X') && (fldBtnState.x8 === 'X')) ||
+			((fldBtnState.x0 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x8 === 'X')) ||
+			((fldBtnState.x2 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x6 === 'X')) ||
+			((fldBtnState.x0 === 'O') && (fldBtnState.x1 === 'O') && (fldBtnState.x2 === 'O')) ||
+			((fldBtnState.x3 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x5 === 'O')) ||
+			((fldBtnState.x6 === 'O') && (fldBtnState.x7 === 'O') && (fldBtnState.x8 === 'O')) ||
+			((fldBtnState.x0 === 'O') && (fldBtnState.x3 === 'O') && (fldBtnState.x6 === 'O')) ||
+			((fldBtnState.x1 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x7 === 'O')) ||
+			((fldBtnState.x2 === 'O') && (fldBtnState.x5 === 'O') && (fldBtnState.x8 === 'O')) ||
+			((fldBtnState.x0 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x8 === 'O')) ||
+			((fldBtnState.x2 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x6 === 'O')) 
+			){
+
+				alert ("We have a winner!");
+			};
 	});
+
 });
--- ticTacToe_workflow_notes.md
+++ ticTacToe_workflow_notes.md
@@ -14,7 +14,8 @@ status 13:30 did a git commint and push...
 	click on button returns the id of the button.
 	time to write some javascript code...	
 
-status 	13
+status 	14:30 basic functionality working... need to recognize a winner,
 	
+status: 16:00  winner logic working... 
 	
 	
