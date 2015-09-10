245fd399f90947b1b826837f403c40fa19385f62
Stephen Deddens <sdeddens@gmail.com>
2015-09-10 15:32:24 -0500

    reset working... game is functional

--- ticTacToe.js
+++ ticTacToe.js
@@ -6,12 +6,24 @@ $(function(){
 // then drive the whole program from the onclick event!
 	
 	var xPlayerState = true;
+	var plays = 0;
 	
 	// create an object to store the field state so we can check for a winner;
 	var fldBtnState = {
-		x0: "click me", x1: "click me", x2: "click me",
-		x3: "click me", x4: "click me", x5: "click me",
-		x6: "click me", x7: "click me", x8: "click me",
+		x0: "", x1: "", x2: "",
+		x3: "", x4: "", x5: "",
+		x6: "", x7: "", x8: "",
+	};
+
+
+	var resetBoard = function () {
+		for (var buttonId in fldBtnState ) {
+			fldBtnState [buttonId] = "";
+			button = $("#"+buttonId);
+			button.html("click me");
+		};
+		plays = 0;
+		xPlayerState = true;
 	};
 
 	$("button.field").on("click", function () {
@@ -27,12 +39,12 @@ $(function(){
 		if (xPlayerState) {
 			button.html("X");
 			fldBtnState[buttonId]="X";
-			xPlayerState = false;
-		} else {
+			xPlayerState = false;}
+		else {
 			button.html("O");
 			fldBtnState[buttonId]="O";
-			xPlayerState = true;
-		};
+			xPlayerState = true;};
+		plays ++;
 		// check for winner... brute force, no feniss!
 		if ( 
 			((fldBtnState.x0 === 'X') && (fldBtnState.x1 === 'X') && (fldBtnState.x2 === 'X')) ||
@@ -42,12 +54,11 @@ $(function(){
 			((fldBtnState.x1 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x7 === 'X')) ||
 			((fldBtnState.x2 === 'X') && (fldBtnState.x5 === 'X') && (fldBtnState.x8 === 'X')) ||
 			((fldBtnState.x0 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x8 === 'X')) ||
-			((fldBtnState.x2 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x6 === 'X'))
-			){
-				alert ("X WINS!");
-		} else {
-
-		if (
+			((fldBtnState.x2 === 'X') && (fldBtnState.x4 === 'X') && (fldBtnState.x6 === 'X')) ){
+				
+			alert ("X WINS!");
+			resetBoard();
+		} else if (
 			((fldBtnState.x0 === 'O') && (fldBtnState.x1 === 'O') && (fldBtnState.x2 === 'O')) ||
 			((fldBtnState.x3 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x5 === 'O')) ||
 			((fldBtnState.x6 === 'O') && (fldBtnState.x7 === 'O') && (fldBtnState.x8 === 'O')) ||
@@ -55,11 +66,13 @@ $(function(){
 			((fldBtnState.x1 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x7 === 'O')) ||
 			((fldBtnState.x2 === 'O') && (fldBtnState.x5 === 'O') && (fldBtnState.x8 === 'O')) ||
 			((fldBtnState.x0 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x8 === 'O')) ||
-			((fldBtnState.x2 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x6 === 'O')) 
-			){
-				alert ("O WINS!");
-			};
+			((fldBtnState.x2 === 'O') && (fldBtnState.x4 === 'O') && (fldBtnState.x6 === 'O')) ){
+
+			alert ("O WINS!");
+			resetBoard();
+		} else if (plays === 9) {
+			alert ("CAT'S GAME!");
+			resetBoard()
 		};
 	});
-
 });
